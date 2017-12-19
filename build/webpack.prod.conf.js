d69e6574cabfb545bd6ad8c14e76b2ba7e6ec02d
Maciej Piosik <m.piosik@gmail.com>
2017-12-19 16:51:45 +0100

    Add _redirects file to root path for Vue SPA hosted on Netlify

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -115,6 +115,11 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../root'),
+        to: config.build.assetsRoot,
+        ignore: ['.*']
       }
     ])
   ]
